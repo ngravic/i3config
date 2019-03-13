97c160f02f34537e74bf7faf944df333cc2ed3ac
Nicolas Grabich <ngravic@gmail.com>
2019-03-13 20:45:44 -0300

    fix script

--- install_script.sh
+++ install_script.sh
@@ -7,13 +7,15 @@ fi
 
 # some vars
 HOME_SUDO=$(eval echo ~$SUDO_USER)
+DISTRO=bionic   #v18
+#DISTRO=xenial  #v16
+APT_REPOS={ "ppa:gnome-terminator", "ppa:jasonpleau/rofi", "ppa:numix/ppa" }
+APT_PATH=/etc/apt/sources.list
 
 # modify apt sources 
-
-APT_PATH=/etc/apt/sources.list
-echo "deb http://ar.archive.ubuntu.com/ubuntu/ bionic main restricted universe multiverse" > $APT_PATH
-echo "deb http://ar.archive.ubuntu.com/ubuntu/ bionic-updates main restricted universe multiverse" >> $APT_PATH
-echo "deb http://ar.archive.ubuntu.com/ubuntu/ bionic-backports main restricted universe multiverse" >> $APT_PATH
+echo "deb http://ar.archive.ubuntu.com/ubuntu/ $DISTRO main restricted universe multiverse" > $APT_PATH
+echo "deb http://ar.archive.ubuntu.com/ubuntu/ $DISTRO-updates main restricted universe multiverse" >> $APT_PATH
+echo "deb http://ar.archive.ubuntu.com/ubuntu/ $DISTRO-backports main restricted universe multiverse" >> $APT_PATH
 echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> $APT_PATH
 echo "deb http://repository.spotify.com stable non-free" >> $APT_PATH
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" >> $APT_PATH
@@ -24,18 +26,21 @@ wget -q -O - https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key ad
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
 
 # add repos
-add-apt-repository ppa:gnome-terminator -n -y
-add-apt-repository ppa:jasonpleau/rofi -n -y
-add-apt-repository ppa:numix/ppa -n -y
-# update apt 
+apt install software-properties-common -y   # needed for add-apt-repository
+for rep in $APT_REPOS; do
+  add-apt-repository $rep -n -y
+done
 
+# update apt 
 apt update
 
 # install common packages
-apt install net-tools ubuntu-drivers-common mesa-utils mesa-utils-extra gnupg numlockx xautolock scrot xorg xserver-xorg xinit wget unzip wpasupplicant software-properties-common vim git apt-transport-https mate-polkit feh galculator thunar thunar-archive-plugin numix-icon-theme-circle -y 
-
-# i3wm, chrome, spotify, vs code
-apt install i3 rofi code google-chrome-stable spotify-client terminator -y
+apt install net-tools ubuntu-drivers-common mesa-utils mesa-utils-extra gnupg \
+  numlockx xautolock scrot xorg xserver-xorg xinit wget unzip wpasupplicant \
+  vim git apt-transport-https mate-polkit feh galculator thunar \
+  thunar-archive-plugin numix-icon-theme-circle i3 rofi code \
+  google-chrome-stable spotify-client terminator \
+  -y 
 
 # install playerctl
 wget http://ftp.nl.debian.org/debian/pool/main/p/playerctl/libplayerctl2_2.0.1-1_amd64.deb -O /tmp/lib.deb
@@ -43,13 +48,13 @@ wget http://ftp.nl.debian.org/debian/pool/main/p/playerctl/playerctl_2.0.1-1_amd
 dpkg -i /tmp/lib.deb /tmp/player.deb
 
 # start i3 when login 
-echo "if [[ -z $DISPLAY ]] && [[ $(tty) = /dev/tty1 ]]; then;startx;fi" > $HOME_SUDO/.bash_profile
-chown $SUDO_USER $HOME_SUDO/.bash_profile
+echo "if [[ -z $DISPLAY ]] && [[ $(tty) = /dev/tty1 ]]; then;startx;fi" > $SUDO_HOME/.bash_profile
+chown $SUDO_USER $SUDO_HOME/.bash_profile
 
 # set icon theme 
-echo "gtk-icon-theme-name=\"Numix-Circle\"" > $HOME_SUDO/.gtkrc-2.0
-echo "gtk-theme-name=\"Breeze-dark-gtk\"" >> $HOME_SUDO/.gtkrc-2.0
-chown $SUDO_USER $HOME_SUDO/.gtkrc-2.0
+echo "gtk-icon-theme-name=\"Numix-Circle\"" > $SUDO_HOME/.gtkrc-2.0
+echo "gtk-theme-name=\"Breeze-dark-gtk\"" >> $SUDO_HOME/.gtkrc-2.0
+chown $SUDO_USER $SUDO_HOME/.gtkrc-2.0
 
 # copy breeze dark theme
 cp -r Breeze-dark-gtk /usr/share/themes/Breeze-dark-gtk
@@ -58,17 +63,17 @@ cp -r Breeze-dark-gtk /usr/share/themes/Breeze-dark-gtk
 cp fontawesome/* /usr/local/share/fonts/
 
 # copy i3 config
-mkdir -p $HOME_SUDO/.config/i3/i3status
-cp i3/config $HOME_SUDO/.config/i3/
-cp i3/i3status/config $HOME_SUDO/.config/i3/i3status/
-chown -R $SUDO_USER $HOME_SUDO/.config/i3
+mkdir -p $SUDO_HOME/.config/i3/i3status
+cp i3/config $SUDO_HOME/.config/i3/
+cp i3/i3status/config $SUDO_HOME/.config/i3/i3status/
+chown -R $SUDO_USER $SUDO_HOME/.config/i3
 
 # copy rofi theme
-mkdir -p $HOME_SUDO/.config/rofi/
-cp rofi/* $HOME_SUDO/.config/rofi/
-chown -R $SUDO_USER $HOME_SUDO/.config/rofi/
+mkdir -p $SUDO_HOME/.config/rofi/
+cp rofi/* $SUDO_HOME/.config/rofi/
+chown -R $SUDO_USER $SUDO_HOME/.config/rofi/
 
 # VS Code custom title bar
-mkdir -p $HOME_SUDO/.config/Code/User
-echo "{ 'window.titleBarStyle' : 'custom' }" > $HOME_SUDO/.config/Code/User/settings.json
-chown -R $SUDO_USER $HOME_SUDO/.config/Code
+mkdir -p $SUDO_HOME/.config/Code/User
+echo "{ 'window.titleBarStyle' : 'custom' }" > $SUDO_HOME/.config/Code/User/settings.json
+chown -R $SUDO_USER $SUDO_HOME/.config/Code
